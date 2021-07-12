5b636eb738d234d1f38a0ed74759afd2cc93d3e8
e9833711_오범석 <lawsnland@gmail.com>
2021-07-12 13:09:28 +0900

    nomad 1-4

--- main.go
+++ main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/lawsn/learngo/something"
 )
+
+func repeateMe(words ...string) {
+	fmt.Println(words)
+}
+
+func lenAndUpper(name string) (int, string) {
+
+	return len(name), strings.ToUpper(name)
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
 
 func main() {
 	fmt.Println("Hello world")
 	something.SayHello()
 	something.SayBye()
+	name := "vincoh"
+	// name = false
 
-	var name string = "vincoh"
-	name = "Lynn"
 	fmt.Println(name)
+
+	fmt.Println(multiply(2, 2))
+
+	totlaLengh, upperName := lenAndUpper(name)
+	fmt.Println(totlaLengh, upperName)
+
+	fmt.Println(lenAndUpper(name))
+
+	repeateMe("vincoh", "vincent", "beomseok", "aaron", "tatiana")
 }
